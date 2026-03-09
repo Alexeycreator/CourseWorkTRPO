276b1220d8038973bce0098830b29a2eb1a1e07b
Alexeycreator <rushev.alexey@gmail.com>
2026-03-09 13:14:39 +0300

    Исправлены скрипты для добавления данных в пустую БД

--- backend/WebApi/SQL/InitClients.sql
+++ backend/WebApi/SQL/InitClients.sql
@@ -1,6 +1,12 @@
-INSERT INTO Clients (SurName, FirstName, MiddleName, PhoneNumber, Email, Login, Password, Passport_Id)
-VALUES ('Иванов', 'Иван', 'Иванович', '+79251234567', 'ivanov@mail.ru', 'ivanov_i', 'Password123', 1),
-       ('Петрова', 'Анна', 'Сергеевна', '89161234567', 'petrova@yandex.ru', 'petrova_a', 'Qwerty456', 2),
-       ('Сидоров', 'Петр', 'Алексеевич', '89031234567', 'sidorov@bk.ru', 'sidorov_p', 'Admin789', 3),
-       ('Козлова', 'Елена', 'Дмитриевна', '+79141234567', 'kozlova@gmail.com', 'kozlova_e', 'TestPass123', 4),
-       ('Морозов', 'Алексей', 'Викторович', '89501234567', 'morozov@list.ru', 'morozov_a', 'StrongPwd1', 5);
+INSERT INTO Clients (SurName, FirstName, MiddleName, PhoneNumber, Email, Login, Password, Passport_Id, Gender, Age,
+                     Birthday)
+VALUES ('Иванов', 'Иван', 'Иванович', '+79251234567', 'ivanov@mail.ru', 'ivanov_i',
+        'Password123', 1, 'Мужской', 32, '09.03.1994'),
+       ('Петрова', 'Анна', 'Сергеевна', '89161234567', 'petrova@yandex.ru', 'petrova_a',
+        'Qwerty456', 2, 'Женский', 40, '09.03.1986'),
+       ('Сидоров', 'Петр', 'Алексеевич', '89031234567', 'sidorov@bk.ru', 'sidorov_p',
+        'Admin789', 3, 'Мужской', 19, '09.03.2007'),
+       ('Козлова', 'Елена', 'Дмитриевна', '+79141234567', 'kozlova@gmail.com', 'kozlova_e',
+        'TestPass123', 4, 'Женский', 29, '09.03.1997'),
+       ('Морозов', 'Алексей', 'Викторович', '89501234567', 'morozov@list.ru', 'morozov_a',
+        'StrongPwd1', 5, 'Мужской', 23, '09.03.2003');
