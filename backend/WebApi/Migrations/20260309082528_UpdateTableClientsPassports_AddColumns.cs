bdea68da1849e556f3ed0814e840adc512fd9e3d
Alexeycreator <rushev.alexey@gmail.com>
2026-03-09 13:18:09 +0300

    Обновлен список миграций

---
+++ backend/WebApi/Migrations/20260309082528_UpdateTableClientsPassports_AddColumns.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableClientsPassports_AddColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Passports",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfIssue",
+                table: "Passports",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentCode",
+                table: "Passports",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IssuedBy",
+                table: "Passports",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Birthday",
+                table: "Clients",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Clients",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfIssue",
+                table: "Passports");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentCode",
+                table: "Passports");
+
+            migrationBuilder.DropColumn(
+                name: "IssuedBy",
+                table: "Passports");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Clients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Passports",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
