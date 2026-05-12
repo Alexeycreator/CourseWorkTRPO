6a67941fd1b40fa0343e55a795ea0b6f99e1e92f
Alexeycreator <rushev.alexey@gmail.com>
2026-05-12 10:58:56 +0300

    Обновлены модели клиента

--- frontend/client-app/src/Services/PassportApi.tsx
+++ frontend/client-app/src/Services/PassportApi.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-throw-literal */
 import axios from "axios";
 import { Address } from "./AddressApi";
 
@@ -23,3 +24,91 @@ export interface Passport {
     placeOfBirth?: string; // ДОБАВЛЕНО: Место рождения
     addresses?: Address[] | null;
 }
+
+export interface CreatePassportDto {
+    seria: number;
+    number: number;
+    type: string;
+    issuedBy: string;
+    departmentCode: string;
+    dateOfIssue: Date;
+};
+
+export interface UpdatePassportDto extends CreatePassportDto {
+    id: number;
+};
+
+export const createPassport = async (userId: number, request: CreatePassportDto): Promise<void> => {
+    try {
+        const response = await api.post('/Passports/create-passport-data', request, {
+            params: { userId }
+        });
+        return response.data;
+    }
+    catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
+            throw { message: error.message, isSetupError: true };
+        }
+    }
+};
+
+export const updatePassport = async (userId: number, request: UpdatePassportDto): Promise<void> => {
+    try {
+        const response = await api.put('/Passports/update-passport', request, {
+            params: { userId }
+        });
+        return response.data;
+    }
+    catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
+            throw { message: error.message, isSetupError: true };
+        }
+    }
+};
+
+export const deletePassport = async (passportId: number, userId: number): Promise<void> => {
+    try {
+        const response = await api.delete('/Passports/delete-passport', {
+            params: {
+                passportId,
+                userId
+            }
+        });
+        return response.data;
+    }
+    catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
+            throw { message: error.message, isSetupError: true };
+        }
+    }
+};
--- frontend/client-app/src/Services/TicketsApi.tsx
+++ frontend/client-app/src/Services/TicketsApi.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-throw-literal */
+import React from "react";
 import axios from "axios";
 
 const API_URL = "http://localhost:5050/api";
@@ -8,3 +10,89 @@ const api = axios.create({
         'Content-Type': 'application/json',
     },
 });
+
+export interface CreateTicketsDto {
+    price: number;
+    departureTime: Date;
+    arrivalTime: Date;
+    dateSale: Date;
+};
+
+export interface UpdateTicketsDto extends CreateTicketsDto {
+    id: number;
+};
+
+export const createTicket = async (userId: number, request: CreateTicketsDto): Promise<void> => {
+    try {
+        const response = await api.post('/Tickets/create-ticket', request, {
+            params: { userId }
+        });
+        return response.data;
+    }
+    catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
+            throw { message: error.message, isSetupError: true };
+        }
+    }
+};
+
+export const updateTicket = async (userId: number, request: UpdateTicketsDto): Promise<void> => {
+    try {
+        const response = await api.put('/Tickets/update-ticket', request, {
+            params: { userId }
+        });
+        return response.data;
+    }
+    catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
+            throw { message: error.message, isSetupError: true };
+        }
+    }
+};
+
+export const deleteTicket = async (ticketId: number, userId: number): Promise<void> => {
+    try {
+        const response = await api.delete('/Tickets/delete-ticket', {
+            params: {
+                ticketId,
+                userId
+            }
+        });
+        return response.data;
+    }
+    catch (error: any) {
+        if (error.response) {
+            console.log('Ошибка ответа:', error.response.data);
+            console.log('Статус:', error.response.status);
+            throw {
+                ...error,
+                serverMessage: error.response.data?.message || 'Неизвестная ошибка',
+                statusCode: error.response.status
+            };
+        } else if (error.request) {
+            throw { message: 'Нет ответа от сервера', isNetworkError: true };
+        } else {
+            throw { message: error.message, isSetupError: true };
+        }
+    }
+};
